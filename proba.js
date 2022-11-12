0306ecbe9dd1a95a0473c05f35443f693ee72672
AndreiApanel <aposha84@gmail.com>
2022-11-12 23:43:39 +0100

    task_if_do_while

--- proba.js
+++ proba.js
@@ -1,16 +1,18 @@
 function testCycle(a, b) {
 	var x;
 	x = '';
-if(a<b) {
-	while (a<=b) {
-x += a + ' ';
-a++;
-}} else {
-	while (b<=a) {
-	x += b +' ';
-	b++;
-	}}
+	if (a<b) {
+do {
+	x += b + ' ';
+	b--;
+  } while (b>=a);
+} else {
+	do {
+	x += a + ' ';
+	a--;
+	 } while (a>=b);
+}
 return console.log(x);
 }
 
-testCycle(6,6)
+testCycle(6,5)
