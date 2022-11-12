4ccadbd6f06cfe4991bc00d034ba072203ceee26
AndreiApanel <aposha84@gmail.com>
2022-11-13 00:44:12 +0100

    task_Math.pow_fpr

--- proba.js
+++ proba.js
@@ -1,18 +1,18 @@
 function testCycle(a, b) {
 	var x;
-	x = '';
+	x = 0;
+	let y = 0;
 	if (a<b) {
-do {
-	x += b + ' ';
-	b--;
-  } while (b>=a);
-} else {
-	do {
-	x += a + ' ';
-	a--;
-	 } while (a>=b);
-}
+			for(let i = a;i<b;i++) {
+		y += Math.pow(i,2);
+		x = y + Math.pow(b,2);
+		}
+		} else { 
+			for(let i = b;i<a;i++) {
+				y += Math.pow(i,2);
+				x = y + Math.pow(a,2);
+				}
+	}
 return console.log(x);
 }
-
-testCycle(6,5)
+testCycle(4,9)
