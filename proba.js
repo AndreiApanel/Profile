ebb910a61cf288d26bfc497628e6b2776c0d22bd
AndreiApanel <aposha84@gmail.com>
2022-12-10 23:49:11 +0100

    by iterating over the divisors of a number.

--- proba.js
+++ proba.js
@@ -1,11 +1,10 @@
-function testCycle (a, b) {
-	var x;
-	while (b) {
-		x = b
-		b = a % b;
-    a = x;
-    
-	}
-	return console.log(x);
+function testCycle (n) {
+	var x = 0;
+for ( x = 2;x < n; x++) {
+	if (n % x == 0) {
+	return 'Составное число';
+	} 
 }
-testCycle (10,99)
+		return 'Простое число';
+}
+console.log (testCycle (3))
