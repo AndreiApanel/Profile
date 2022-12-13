db3ec2cd6d81fe7b39faafaf212f407a90d3188a
AndreiApanel <aposha84@gmail.com>
2022-12-13 12:33:00 +0100

    решетом Эратосфена

--- proba.js
+++ proba.js
@@ -1,10 +1,20 @@
+// // Дано число n (n > 1). Выведите строку, состоящую из 
+// простых чисел, меньших n. Воспользуйтесь решетом Эратосфена. 
+// Единицу не считаем простым числом.
 function testCycle (n) {
-	var x = 0;
-for ( x = 2;x < n; x++) {
-	if (n % x == 0) {
-	return 'Составное число';
-	} 
+	var x = [];
+	var y = 2;
+	for (let i = 2;i < n; i++) {x [i] = true;}
+	do {
+		for (i = 2 * y; i<n; i+=y) {x[i] = false;}
+		for (i = y+1;i < n;i++) {
+			if (x[i]) break;
+	}
+	y = i;
+} while (y*y<n);
+var sum = '';
+for (i = 0; i < x.length; i++) {
+if (x[i]) {sum += ' ' +i;}
+} return sum
 }
-		return 'Простое число';
-}
-console.log (testCycle (3))
+console.log (testCycle (27))
