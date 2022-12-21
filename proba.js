827980d0dc021ccfbe0e84c7d6959b21264c6b86
AndreiApanel <aposha84@gmail.com>
2022-12-21 23:04:11 +0100

    newTask 13

--- proba.js
+++ proba.js
@@ -1,20 +1,13 @@
-// // Дано число n (n > 1). Выведите строку, состоящую из 
-// простых чисел, меньших n. Воспользуйтесь решетом Эратосфена. 
-// Единицу не считаем простым числом.
+// Дано число n. Найдите сумму его цифр.
+// (Возможно, функция Math.trunc окажется полезной)
 function testCycle (n) {
-	var x = [];
-	var y = 2;
-	for (let i = 2;i < n; i++) {x [i] = true;}
-	do {
-		for (i = 2 * y; i<n; i+=y) {x[i] = false;}
-		for (i = y+1;i < n;i++) {
-			if (x[i]) break;
-	}
-	y = i;
-} while (y*y<n);
-var sum = '';
-for (i = 0; i < x.length; i++) {
-if (x[i]) {sum += ' ' +i;}
-} return sum
+	var x = 0;
+	var y = 0;
+	while (n) {
+	y = n % 10;
+	n = (n - y)/10;
+	x += y;
 }
-console.log (testCycle (27))
+return x;
+}
+console.log (testCycle(2260))
