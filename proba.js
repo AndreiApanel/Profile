6e3e74b5d6492a24b2122c7fd935530498f110a3
AndreiApanel <aposha84@gmail.com>
2023-01-06 18:08:13 +0100

    add:  0.Quarter of the year

--- proba.js
+++ proba.js
@@ -1,19 +1,25 @@
-// // Дано число n (n > 1). Верните из функции n-ное число Фибоначчи
-// Для наглядности прикладываю последовательность чисел Фиббоначе:
-// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
-//  1597, 2584, 4181, 6765, 10946, 17711, …
-// Например дано число 4, при отсчете слева, 
-// начиная с "1", четвертым по счету будет число "3",
-//  а если дано "7", то 7-м по счету числом является число "13".
-function testCycle(n) { 
-var x = 1;
-var	y = 1;
-var c = 0; 
-	for(i = 3; i <= n; i++) { 
-		c = x + y;
-		x = y; 
-		y = c;
-	}
-	return y;
-}
-console.log (testCycle(7))
+/* Given a month as an integer from 1 to 12,
+// return to which quarter of the year it belongs
+// as an integer number.
+// For example: month 2 (February),
+// is part of the first quarter; month 6 (June),
+//is part of the second quarter; and month 11 (November),
+is part of the fourth quarter.*/
+const quarterOf = (month) => {
+	for (i = 1, i<=month,i++) {
+		if (i < 3) {
+			return quarter + '1'
+		}
+		if (3<i<=6) {
+			return quarter + '2'
+		}
+		if (6<i<=9) {
+			return quarter + '3'
+		}
+		if (9<i<=12) {
+			return quarter + '4'
+		}
+
+	return Data.month(month)
+}}
+quarterOf(2)
