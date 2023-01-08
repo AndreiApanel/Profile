6dbf0f348293ce11bde0e49b756c64582b767850
AndreiApanel <aposha84@gmail.com>
2023-01-08 19:57:17 +0100

    8.Function 1 - hello world

--- proba.js
+++ proba.js
@@ -1,17 +1,9 @@
-// Create a function that takes an integer as an argument 
-// and returns "Even" for even numbers or "Odd" for odd numbers.
-function fakeBin(x){
-let y = Array.from(x);
-let c = [];
-y.forEach(arguments => {
-	if(arguments < 5) {
-	return c.push(0);
-	}
-	if (arguments>=5){
-	return c.push(1);
-	}
-	}
-	)
-	return c.join('');
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".Style Points
+// Sure, this is about as easy as it gets.
+//  But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+function greet (x) {
+return ('hello world!')
 }
-console.log(fakeBin('564668480227'));
+console.log(greet(12));
