fe80dafa9ef483a33be5b6739fa229209ab44284
AndreiApanel <aposha84@gmail.com>
2023-01-11 16:22:23 +0100

    9.Largest Square Inside A Circle

--- proba.js
+++ proba.js
@@ -1,9 +1,7 @@
-// Description:
-// Make a simple function called greet that returns the most-famous "hello world!".Style Points
-// Sure, this is about as easy as it gets.
-//  But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+// Determine the area of the largest square that can fit inside a circle with radius r.
 
-function greet (x) {
-return ('hello world!')
+function areaLargestSquare(r) {
+	let s;
+  return s = 2*Math.pow(r,2);
 }
-console.log(greet(12));
+console.log(areaLargestSquare(4));
