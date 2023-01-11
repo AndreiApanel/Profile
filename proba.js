d62e07c1f8f22fef7c24625daf2ef220cedbb456
AndreiApanel <aposha84@gmail.com>
2023-01-11 18:06:32 +0100

    14.Remove String Spaces

--- proba.js
+++ proba.js
@@ -1,6 +1,5 @@
-// It's pretty straightforward. 
-// Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
-function removeChar(str){
-return str.slice(1,-1)
- };
-console.log(removeChar('chat'))
+// Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x){
+return x = x.replace(/ /g,'')
+}
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
