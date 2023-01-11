dc781f3f59b91cfbe9eef65d4c16239b81962c32
AndreiApanel <aposha84@gmail.com>
2023-01-11 19:45:06 +0100

    18.Student's Final Grade

--- proba.js
+++ proba.js
@@ -1,6 +1,22 @@
-// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
-function repeatStr (n, s) {
-
-return s.repeat(n)
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+// This function should return a number (final grade). There are four types of final grades:
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+function finalGrade (exam, projects) {
+	if (exam > 90 || projects > 10) {
+		return 100
+	}
+	if (75 < exam && 5 <= projects) {
+	return 90
+	}
+	if (50 < exam && 2 <= projects) {
+	return 75
+	}
+	else {
+	return 0
+	}
 }
-console.log(repeatStr(3, "*"))
+console.log(finalGrade(90, 5))
