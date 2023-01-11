62d1a33dd4509a8619e7cdaaf08561e868e2ae3d
AndreiApanel <aposha84@gmail.com>
2023-01-11 16:57:44 +0100

    10.Number of Decimal Digits

--- proba.js
+++ proba.js
@@ -1,7 +1,13 @@
-// Determine the area of the largest square that can fit inside a circle with radius r.
+// Determine the total number of digits in the integer (n>=0) 
+// given as input to the function. For example, 
+// 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
 
-function areaLargestSquare(r) {
-	let s;
-  return s = 2*Math.pow(r,2);
+function digits(n) {
+if (n > 0 ) {
+let i = Math.log(10) 
+return Math.floor(Math.log(n) / i) + 1}
+else {
+let x = n + 1
+return x}
 }
-console.log(areaLargestSquare(4));
+console.log(digits(0));
