d1a3034fe049b9df6bd8110e842b4a71b36a483a
AndreiApanel <aposha84@gmail.com>
2023-01-11 19:59:44 +0100

    20.Thinkful - Logic Drills: Traffic light

--- proba.js
+++ proba.js
@@ -1,29 +1,13 @@
-// When provided with a number between 0-9, return it in words.
-// Input :: 1
-// Output :: "One".
-// If your language supports it, try using a switch statement.
-function switchItUp(number){
-switch(number) {
-case 1:
-	return "One";
-case 2:
-	return "Two"
-case 3:
-	return "Three";
-case 4:
-	return "Four";
-case 5:
-	return "Five";
-case 6:
-return "Six";
-case 7:
-return "Seven";
-case 8:
-return "Eight";
-case 9:
-return "Nine";
-case 0:
-return "Zero";
-}
-}
-console.log(switchItUp(1))
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// For example, when the input is green, output should be yellow.
+function updateLight(current) {
+switch (current) {
+case 'green':
+return 'yellow'
+case 'yellow':
+return 'red'
+case 'red':
+return 'green'
+}}
+console.log(updateLight('green'))
