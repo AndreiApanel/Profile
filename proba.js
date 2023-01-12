50572f9a53b028dd86ab250fedd12e7b06dd8771
AndreiApanel <aposha84@gmail.com>
2023-01-12 18:14:04 +0100

    24.Will you make it?

--- proba.js
+++ proba.js
@@ -1,6 +1,10 @@
-// Return the type of the sum of the two arguments
-function typeOfSum(a, b) {
-let c = a + b;
-return typeof(c)
-}
-console.log(typeOfSum(12, 1))
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+// Function should return true if it is possible and false if not.
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+	if (distanceToPump/25<=(mpg*fuelLeft)/25) {
+return true
+	}
+	return false
+};
+console.log(zeroFuel(100, 50, 1))
