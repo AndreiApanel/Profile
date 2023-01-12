c35c2b57de45096eb1e8ff312e449f4fea67bb51
AndreiApanel <aposha84@gmail.com>
2023-01-12 16:52:12 +0100

    21.Third Angle of a Triangle

--- proba.js
+++ proba.js
@@ -1,13 +1,11 @@
-// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
-// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
-// For example, when the input is green, output should be yellow.
-function updateLight(current) {
-switch (current) {
-case 'green':
-return 'yellow'
-case 'yellow':
-return 'red'
-case 'red':
-return 'green'
-}}
-console.log(updateLight('green'))
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+function otherAngle(a, b) {
+	let c;
+	let third;
+	c = a + b;
+	third = 180 - c;
+  return third;
+}
+console.log(otherAngle(30, 60))
