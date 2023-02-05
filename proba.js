de260152c84dbff403b13b6e15a6a7b8e074a47f
AndreiApanel <aposha84@gmail.com>
2023-02-05 21:15:22 +0100

    Bind, apply,call : methods

--- proba.js
+++ proba.js
@@ -1,18 +1,43 @@
-function testCycle(a, b) {
-	var x;
-	x = 0;
-	let y = 0;
-	if (a<b) {
-			for(let i = a;i<b;i++) {
-		y += Math.pow(i,2);
-		x = y + Math.pow(b,2);
-		}
-		} else { 
-			for(let i = b;i<a;i++) {
-				y += Math.pow(i,2);
-				x = y + Math.pow(a,2);
-				}
-	}
-return console.log(x);
+function hello() {
+	console.log('hello', this);
 }
-testCycle(4,9)
+
+const person = {
+	name: 'John',
+	age: 25,
+	sayHello: hello,
+	sayHelloWindow: hello.bind( ),
+	logInfo: function(job, phone) {
+		console.group(`${this.name} info:`)
+		console.log(`Name is ${this.name}`);
+		console.log(`age is ${this.age}`);
+		console.log(`Job is ${job}`);
+		console.log(`Phone is ${phone}`);
+		console.groupEnd();
+	}
+}
+const lena = {
+	name: 'Lena',
+	age: 25,
+}
+// person.logInfo.call(lena,'frontend','80-9-09-9-90')
+// person.logInfo.bind(lena,'frontend','80-9-09-9-90')();
+
+// person.logInfo.apply(lena, ['frontend','80-9-09-9-90'])
+
+const array = [1,2,3,4,5]
+
+// function multBy (arr,n) {
+// 	return arr.map(function (i) {
+// 		return i * n
+// 	});
+// }
+// console.log(multBy(array,5))
+
+Array.prototype.multBy = function (n) {
+	return this.map(function (i) {
+		return i * n
+	});
+}
+
+console.log(array.multBy(2))
