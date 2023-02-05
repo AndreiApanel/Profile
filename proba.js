fdb088e20826116d3b7d6ce589a38bf0e4cb40ed
AndreiApanel <aposha84@gmail.com>
2023-02-05 13:05:32 +0100

    01 Count strings in objects

--- proba.js
+++ proba.js
@@ -1,18 +1,17 @@
-function testCycle(a, b) {
-	var x;
-	x = 0;
-	let y = 0;
-	if (a<b) {
-			for(let i = a;i<b;i++) {
-		y += Math.pow(i,2);
-		x = y + Math.pow(b,2);
-		}
-		} else { 
-			for(let i = b;i<a;i++) {
-				y += Math.pow(i,2);
-				x = y + Math.pow(a,2);
+// Create a function strCount (takes an object as argument) that will count all string values inside an object
+function strCount(obj) {
+	let x = 0;
+	function count(object) {
+		for (let key in object) {
+			if (typeof object[key] === 'object') {
+				count(object[key]);
+			} else {
+				if (typeof object[key] === 'string') {
+					x++;
 				}
+			}
+		}
 	}
-return console.log(x);
+	count(obj);
+	return x;
 }
-testCycle(4,9)
