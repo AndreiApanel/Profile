56e96700254fd5a6154d2b7930f4c775b9a02275
AndreiApanel <aposha84@gmail.com>
2023-02-13 13:41:04 +0100

    Fetch, Promise

--- proba.js
+++ proba.js
@@ -1,44 +1,20 @@
-function hello() {
-  console.log("hello", this);
+const delay = (ms) => {
+  return new Promise((r) => setTimeout(() => r(), ms));
+};
+
+delay(2000).then(() => console.log("2 sec"));
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+function fetchTodos() {
+  console.log("Fetch todo started...");
+
+  return delay(2000)
+    .then(() => fetch(url))
+    .then((response) => response.json());
 }
-
-const person = {
-  name: "John",
-  age: 25,
-  sayHello: hello,
-  sayHelloWindow: hello.bind(),
-  logInfo: function (job, phone) {
-    console.group(`${this.name} info:`);
-    console.log(`Name is ${this.name}`);
-    console.log(`age is ${this.age}`);
-    console.log(`Job is ${job}`);
-    console.log(`Phone is ${phone}`);
-    console.groupEnd();
-  },
-};
-const lena = {
-  name: "Lena",
-  age: 25,
-};
-// person.logInfo.call(lena,'frontend','80-9-09-9-90')
-// person.logInfo.bind(lena,'frontend','80-9-09-9-90')();
-
-// person.logInfo.apply(lena, ['frontend','80-9-09-9-90'])
-
-const array = [1, 2, 3, 4, 5];
-
-// function multBy (arr,n) {
-// 	return arr.map(function (i) {
-// 		return i * n
-// 	});
-// }
-// console.log(multBy(array,5))
-
-Array.prototype.multBy = function (n) {
-  return this.map(function (i) {
-    return i * n;
-  });
-};
-
-console.log(array.multBy(2));
-console.log("");
+fetchTodos()
+  .then((data) => {
+    console.log("Data:", data);
+  })
+  .catch((e) => console.error(e));
