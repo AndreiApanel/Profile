256071fbaeff180ef61bcd1d044517ed45de3c14
AndreiApanel <aposha84@gmail.com>
2023-02-26 19:40:53 +0100

    2. 16+18=214

--- proba.js
+++ proba.js
@@ -1,15 +1,16 @@
-// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+// For this kata you will have to forget how to add two numbers.
+// In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
 
-// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+// You may assume both integers are positive integers.
+function add(num1, num2) {
+  num1 = num1.toString().split("").reverse();
+  num2 = num2.toString().split("").reverse();
+  let sum = [];
+  let length = Math.max(num1.length, num2.length);
+  for (let j = length - 1; j >= 0; j--) {
+    sum.push(Number(num1[j] || 0) + Number(num2[j] || 0));
+  }
 
-// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
-function mergeArrays(arr1, arr2) {
-  arr1 = typeof arr1 !== "undefined" ? arr1 : [];
-  arr2 = typeof arr2 !== "undefined" ? arr2 : [];
-  let arr3 = [].concat(arr1, arr2).sort((a, b) => a - b);
-  let newSet = new Set(arr3);
-  let arr4 = Array.from(newSet);
-  return arr4;
+  return +sum.join("");
 }
-
-console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]));
+console.log(add(122, 81));
