95e4ebe132c42978051d1bca39c7a6844b2cdca3
AndreiApanel <aposha84@gmail.com>
2023-02-26 22:10:20 +0100

    12.Odd-Even String Sort

--- proba.js
+++ proba.js
@@ -1,25 +1,10 @@
-/*
-Description:
-Nickname Generator
-Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
-If the 3rd letter is a consonant, return the first 3 letters.
-nickname("Robert") //=> "Rob"
-nickname("Kimberly") //=> "Kim"
-nickname("Samantha") //=> "Sam"
-If the 3rd letter is a vowel, return the first 4 letters.
-nickname("Jeannie") //=> "Jean"
-nickname("Douglas") //=> "Doug"
-nickname("Gregory") //=> "Greg"
-If the string is less than 4 characters, return "Error: Name too short".
-Notes:
-Vowels are "aeiou", so discount the letter "y".
-Input will always be a string.
-Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
-The input can be modified
-*/
-
-function nicknameGenerator(name) {
-  if (name.length < 4) return "Error: Name too short";
-  if (name[2].match(/[aeuio]/)) return name.slice(0, 4);
-  return name.slice(0, 3);
+// Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+function sortMyString(S) {
+  let even = S.split("")
+    .filter((v, i) => i % 2 === 0)
+    .join("");
+  let odd = S.split("")
+    .filter((v, i) => i % 2 !== 0)
+    .join("");
+  return even + " " + odd;
 }
