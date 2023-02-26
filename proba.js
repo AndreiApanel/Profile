533af8b4dcd25b1c8fb62d536a8660515378d23c
AndreiApanel <aposha84@gmail.com>
2023-02-26 21:34:08 +0100

    4.Colour Association

--- proba.js
+++ proba.js
@@ -1,4 +1,24 @@
 // Your task is to add a new property users Answer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
-questions.forEach(function (q) {
-  q.usersAnswer = null;
-});
+function colourAssociation(array) {
+  let result = [];
+  for (let index = 0; index < array.length; index++) {
+    result[index] = { [array[index][0]]: array[index][1] };
+  }
+
+  return result;
+}
+
+console.log(
+  colourAssociation([
+    ["yellow", "flexible"],
+    ["blue", "humerous"],
+    ["orange", "lazy"],
+    ["pink", "dramatic"],
+    ["blue", "mystical"],
+    ["yellow", "angry"],
+    ["pink", "dramatic"],
+    ["white", "humerous"],
+    ["red", "lazy"],
+    ["white", "mystical"],
+  ])
+);
