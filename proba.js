102f4fc844d42a08512bf3629139fd3f92aa864a
AndreiApanel <aposha84@gmail.com>
2023-02-26 22:25:57 +0100

    15.Minimize Sum Of Array (Array Series #1)

--- proba.js
+++ proba.js
@@ -1,27 +1,11 @@
-/*
-Description:
-A stream of data is received and needs to be reversed.
-Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
-11111111  00000000  00001111  10101010
- (byte1)   (byte2)   (byte3)   (byte4)
-should become:
-10101010  00001111  00000000  11111111
- (byte4)   (byte3)   (byte2)   (byte1)
-The total number of bits will always be a multiple of 8.
-The data is given in an array as such:
-[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
-*/
+// Task
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
 
-const dataReverse = function (data) {
-  if (data.length === 0) return [];
-  const arrayBytes = [];
-  const tam = data.length / 8;
-  for (var i = 0; i < tam; i++) {
-    arrayBytes.push(data.splice(0, 8));
+function minSum(arr) {
+  arr = arr.slice().sort((a, b) => a - b);
+  let sum = 0;
+  while (arr.length) {
+    sum += arr.shift() * arr.pop();
   }
-  return arrayBytes
-    .reverse()
-    .join()
-    .split(",")
-    .map((v) => v * 1);
-};
+  return sum;
+}
